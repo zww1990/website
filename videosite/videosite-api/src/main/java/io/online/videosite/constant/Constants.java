4b2dea7b90ca8a0848094cbf65bfab74e16f4ca0
zww1990 <zww1990@foxmail.com>
2024-03-08 09:59:29 +0800

    new file:   Constants.java

---
+++ videosite/videosite-api/src/main/java/io/online/videosite/constant/Constants.java
@@ -0,0 +1,15 @@
+package io.online.videosite.constant;
+
+/**
+ * 自定义常量类
+ *
+ * @author 张维维
+ */
+public interface Constants {
+    /**
+     * 会话中当前登录用户的键
+     */
+    String SESSION_USER_KEY = "CURRENT_USER";
+    String JSONFORMAT_DATETIME = "yyyy-MM-dd HH:mm:ss";
+    String JSONFORMAT_TIMEZONE = "GMT+8";
+}
