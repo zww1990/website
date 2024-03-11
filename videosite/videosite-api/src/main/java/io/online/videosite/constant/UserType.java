c516fbc03691c4613454157d511df0ea9d079e65
zww1990 <zww1990@foxmail.com>
2024-03-11 12:03:27 +0800

    new file:UserType.java

---
+++ videosite/videosite-api/src/main/java/io/online/videosite/constant/UserType.java
@@ -0,0 +1,17 @@
+package io.online.videosite.constant;
+
+/**
+ * 用户类型枚举类
+ *
+ * @author 张维维
+ */
+public enum UserType {
+    /**
+     * 系统管理员
+     */
+    ADMIN,
+    /**
+     * 普通用户
+     */
+    NORMAL,
+}
