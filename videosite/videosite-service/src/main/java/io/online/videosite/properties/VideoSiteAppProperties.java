ba468f0bb0903cb97f6c790e59571631bb012c2b
zww1990 <zww1990@foxmail.com>
2024-03-29 10:27:33 +0800

    new file:   VideoSiteAppProperties.java

---
+++ videosite/videosite-service/src/main/java/io/online/videosite/properties/VideoSiteAppProperties.java
@@ -0,0 +1,48 @@
+package io.online.videosite.properties;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 自定义配置属性类
+ *
+ * @author 张维维
+ */
+@Configuration
+@ConfigurationProperties(ignoreInvalidFields = true, prefix = "videosite-app")
+@Getter
+@Setter
+@ToString
+public class VideoSiteAppProperties {
+    /**
+     * 拦截器包括的路径模式
+     */
+    private String[] includePathPatterns = {"/category/**", "/comment/**", "/videohub/**", "/user/list"};
+    /**
+     * 拦截器排除的路径模式
+     */
+    private String[] excludePathPatterns = {"/videohub/show/**", "/category/list"};
+    /**
+     * 需要管理员身份的路径模式
+     */
+    private String[] adminPathPatterns = {"/category/**", "/videohub/audit/**", "/user/list"};
+    /**
+     * 图片格式模式
+     */
+    private String[] imageMimePatterns = {"image/*"};
+    /**
+     * 视频格式模式
+     */
+    private String[] videoMimePatterns = {"video/*"};
+    /**
+     * 图片上传目录
+     */
+    private String imageUploadFolder = "/upload/images";
+    /**
+     * 视频上传目录
+     */
+    private String videoUploadFolder = "/upload/videos";
+}
