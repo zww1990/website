d1c85c69e2352d55e3c969e2da9cefac69240f30
zww1990 <zww1990@foxmail.com>
2024-04-01 10:03:26 +0800

    new file:   CategoryRepository.java

---
+++ videosite/videosite-service/src/main/java/io/online/videosite/repository/CategoryRepository.java
@@ -0,0 +1,13 @@
+package io.online.videosite.repository;
+
+import io.online.videosite.domain.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+/**
+ * 视频类别数据访问对象接口
+ *
+ * @author 张维维
+ */
+public interface CategoryRepository extends JpaRepository<Category, Integer>, JpaSpecificationExecutor<Category> {
+}
