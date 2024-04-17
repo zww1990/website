a12a287f10fb91f484415eb8f90e2fe635ecb7eb
zww1990 <zww1990@foxmail.com>
2024-04-17 09:47:24 +0800

    new file:   VideoSiteAppConfig.java

---
+++ videosite/videosite-web/src/main/java/io/online/videosite/config/VideoSiteAppConfig.java
@@ -0,0 +1,96 @@
+package io.online.videosite.config;
+
+import io.online.videosite.properties.VideoSiteAppProperties;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.web.server.ErrorPage;
+import org.springframework.boot.web.server.ErrorPageRegistrar;
+import org.springframework.boot.web.server.ErrorPageRegistry;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.util.UrlPathHelper;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+/**
+ * 自定义Spring MVC配置类
+ *
+ * @author 张维维
+ */
+@Configuration
+@AllArgsConstructor
+@Slf4j
+public class VideoSiteAppConfig implements WebMvcConfigurer, CommandLineRunner, ErrorPageRegistrar {
+    private final VideoSiteAppProperties appProps;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        log.info("{}", this.appProps);
+        registry.addInterceptor(authenticationInterceptor())
+                .addPathPatterns(this.appProps.getIncludePathPatterns())
+                .excludePathPatterns(this.appProps.getExcludePathPatterns());
+    }
+
+    @Override
+    public void configurePathMatch(PathMatchConfigurer configurer) {
+        UrlPathHelper urlPathHelper = new UrlPathHelper();
+        urlPathHelper.setRemoveSemicolonContent(false);
+        configurer.setUrlPathHelper(urlPathHelper);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowCredentials(true)
+                .allowedHeaders(CorsConfiguration.ALL)
+                .allowedMethods(CorsConfiguration.ALL)
+                .allowedOriginPatterns(CorsConfiguration.ALL)
+                .exposedHeaders(CorsConfiguration.ALL);
+    }
+
+    /**
+     * 注册身份验证拦截器
+     */
+    @Bean
+    AuthenticationInterceptor authenticationInterceptor() {
+        return new AuthenticationInterceptor();
+    }
+
+    /**
+     * 注册密码编码器
+     */
+    @Bean
+    static PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Path image = Paths.get(this.appProps.getImageUploadFolder());
+        if (Files.notExists(image)) {
+            // 创建上传图片目录
+            Files.createDirectories(image);
+        }
+        Path video = Paths.get(this.appProps.getVideoUploadFolder());
+        if (Files.notExists(video)) {
+            // 创建上传视频目录
+            Files.createDirectories(video);
+        }
+    }
+
+    @Override
+    public void registerErrorPages(ErrorPageRegistry registry) {
+        registry.addErrorPages(new ErrorPage(HttpStatus.NOT_FOUND, "/index.html"));
+    }
+}
