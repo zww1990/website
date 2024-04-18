7d03227bf4a45af6a07e3794e55baaf4d1e5f3d5
zww1990 <zww1990@foxmail.com>
2024-04-18 09:28:14 +0800

    new file:   CategoryController.java

---
+++ videosite/videosite-web/src/main/java/io/online/videosite/controller/CategoryController.java
@@ -0,0 +1,53 @@
+package io.online.videosite.controller;
+
+import io.online.videosite.api.CategoryService;
+import io.online.videosite.constant.Constants;
+import io.online.videosite.domain.Category;
+import io.online.videosite.domain.User;
+import lombok.AllArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * 视频类别控制器
+ *
+ * @author 张维维
+ */
+@RestController
+@RequestMapping(path = "/category")
+@AllArgsConstructor
+public class CategoryController {
+    private final CategoryService categoryService;
+
+    /**
+     * 处理添加类别
+     */
+    @PostMapping(path = "/add")
+    public ResponseEntity<?> handleAdd(@RequestBody Category category,
+                                       @SessionAttribute(Constants.SESSION_USER_KEY) User user) {
+        if (!StringUtils.hasText(category.getCategoryName())) {
+            return ResponseEntity.badRequest()
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .body("请输入视频类别名称！");
+        }
+        if (this.categoryService.checkExist(category)) {
+            return ResponseEntity.badRequest()
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .body(String.format("此 %s 已存在！", category.getCategoryName()));
+        }
+        this.categoryService.save(category, user);
+        return ResponseEntity.ok()
+                .contentType(MediaType.APPLICATION_JSON)
+                .body("添加成功！");
+    }
+
+    /**
+     * 类别列表
+     */
+    @GetMapping(path = "/list")
+    public ResponseEntity<?> list() {
+        return ResponseEntity.ok(this.categoryService.query());
+    }
+}
