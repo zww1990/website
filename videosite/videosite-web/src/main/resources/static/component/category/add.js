c77046fc4350302d2e2616156e5ae645f5918480
zww1990 <zww1990@foxmail.com>
2024-05-10 08:59:29 +0800

    new file:   add.js

---
+++ videosite/videosite-web/src/main/resources/static/component/category/add.js
@@ -0,0 +1,46 @@
+import { categoryAddApi } from '../utils/api.js'
+
+const { ref, reactive } = Vue
+const { message } = antd
+
+export default {
+  setup() {
+    const router = VueRouter.useRouter()
+    const formState = reactive({ categoryName: '' })
+    const onFinish = async values => {
+      const res = await categoryAddApi(values)
+      if(res.ok){
+        router.push('/cate/success')
+      }else{
+        message.error(await res.text())
+      }
+    }
+    return { onFinish, formState }
+  },
+  template: `
+    <a-form
+      :model="formState"
+      name="basic"
+      :label-col="{ span: 10 }"
+      :wrapper-col="{ span: 4 }"
+      autocomplete="off"
+      @finish="onFinish"
+    >
+      <a-form-item></a-form-item>
+      <a-form-item></a-form-item>
+      <a-form-item></a-form-item>
+      <a-form-item
+        has-feedback
+        label="视频类别名称"
+        name="categoryName"
+        :rules="[{ required: true, message: '请输入视频类别名称!' }]"
+      >
+        <a-input v-model:value="formState.categoryName" />
+      </a-form-item>
+
+      <a-form-item :wrapper-col="{ offset: 10, span: 4 }">
+        <a-button type="primary" html-type="submit">添加</a-button>
+      </a-form-item>
+    </a-form>
+  `
+}
