3c2572de3f3939c694d70d329509f373a18520fb
zww1990 <zww1990@foxmail.com>
2024-05-11 09:01:31 +0800

    new file:   success.js

---
+++ videosite/videosite-web/src/main/resources/static/component/category/success.js
@@ -0,0 +1,20 @@
+export default {
+  setup() {
+    const router = VueRouter.useRouter()
+    const goCategoryAdd = () => {
+      router.push('/cate/add')
+    }
+    const goHome = () => {
+      router.push('/')
+    }
+    return { goCategoryAdd, goHome }
+  },
+  template: `
+    <a-result status="success" title="视频类别添加成功！">
+      <template #extra>
+        <a-button @click="goCategoryAdd" type="primary">继续添加</a-button>
+        <a-button @click="goHome">返回主页</a-button>
+      </template>
+    </a-result>
+  `
+}
