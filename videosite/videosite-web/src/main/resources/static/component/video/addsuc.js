13a4a6f4b389aabf7ca3000a87902eac370251fd
zww1990 <zww1990@foxmail.com>
2024-05-22 08:52:17 +0800

    new file:   addsuc.js

---
+++ videosite/videosite-web/src/main/resources/static/component/video/addsuc.js
@@ -0,0 +1,20 @@
+export default {
+  setup() {
+    const router = VueRouter.useRouter()
+    const goVideoAdd = () => {
+      router.push('/video/add')
+    }
+    const goHome = () => {
+      router.push('/')
+    }
+    return { goVideoAdd, goHome }
+  },
+  template: `
+    <a-result status="success" title="视频添加成功！">
+      <template #extra>
+        <a-button @click="goVideoAdd" type="primary">继续添加</a-button>
+        <a-button @click="goHome">返回主页</a-button>
+      </template>
+    </a-result>
+  `
+}
