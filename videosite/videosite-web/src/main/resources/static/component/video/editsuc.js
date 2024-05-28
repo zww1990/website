cc977a2ce39d6ea23ed2efdd24c64552d8263535
zww1990 <zww1990@foxmail.com>
2024-05-28 08:55:07 +0800

    new file:   editsuc.js

---
+++ videosite/videosite-web/src/main/resources/static/component/video/editsuc.js
@@ -0,0 +1,20 @@
+export default {
+  setup() {
+    const router = VueRouter.useRouter()
+    const goVideoList = () => {
+      router.push('/video/list')
+    }
+    const goHome = () => {
+      router.push('/')
+    }
+    return { goVideoList, goHome }
+  },
+  template: `
+    <a-result status="success" title="视频编辑成功！">
+      <template #extra>
+        <a-button @click="goVideoList" type="primary">视频列表</a-button>
+        <a-button @click="goHome">返回主页</a-button>
+      </template>
+    </a-result>
+  `
+}
