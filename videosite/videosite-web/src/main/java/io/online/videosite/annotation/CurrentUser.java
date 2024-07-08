86305bf8c939411fb727cff45ec18b9167f59759
zww1990 <zww1990@foxmail.com>
2024-07-08 15:19:48 +0800

    自定义注解：获取当前认证的用户。

---
+++ videosite/videosite-web/src/main/java/io/online/videosite/annotation/CurrentUser.java
@@ -0,0 +1,18 @@
+package io.online.videosite.annotation;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+
+import java.lang.annotation.*;
+
+/**
+ * 自定义注解：获取当前认证的用户
+ *
+ * @author 张维维
+ * @since 2024-07-08 15:17:48
+ */
+@AuthenticationPrincipal
+@Target({ElementType.PARAMETER, ElementType.ANNOTATION_TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface CurrentUser {
+}
