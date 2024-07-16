a2122aaf060401f8694efb02a1bb83f9909a3a3e
zww1990 <zww1990@foxmail.com>
2024-07-16 20:48:23 +0800

    优化代码

--- videosite/videosite-web/src/main/resources/static/component/user/list.js
+++ videosite/videosite-web/src/main/resources/static/component/user/list.js
@@ -3,15 +3,11 @@ import { userListApi } from '../utils/axiosapi.js'
 export default {
   async setup() {
     const dataSource = await userListApi()
-    dataSource.forEach(it => {
-        let authNames = it.authorities.map(jt => jt.authName)
-        Object.assign(it, { userType: authNames.join(',') })
-    })
     const columns = [
       { title: '主键', dataIndex: 'id', },
       { title: '用户名', dataIndex: 'username', },
       { title: '昵称', dataIndex: 'nickname', },
-      { title: '类型', dataIndex: 'userType', },
+      { title: '类型', customRender: ({record}) => record.authorities.map(it => it.authName).join(',') },
       { title: '创建人', dataIndex: 'creator', },
       { title: '创建时间', dataIndex: 'createdDate', },
       { title: '修改人', dataIndex: 'modifier', },
