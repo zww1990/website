4accedcc4f2bae6606b5230824f6dddc6bb998dd
zww1990 <zww1990@foxmail.com>
2024-07-24 17:26:25 +0800

    JwtTests.java

--- videosite/videosite-web/src/test/java/io/online/videosite/JwtTests.java
+++ videosite/videosite-web/src/test/java/io/online/videosite/JwtTests.java
@@ -1,69 +1,56 @@
 package io.online.videosite;
 
-import com.nimbusds.jose.JWSAlgorithm;
-import com.nimbusds.jose.JWSHeader;
-import com.nimbusds.jose.crypto.RSASSASigner;
-import com.nimbusds.jose.crypto.RSASSAVerifier;
-import com.nimbusds.jose.jwk.RSAKey;
-import com.nimbusds.jose.jwk.gen.RSAKeyGenerator;
-import com.nimbusds.jwt.JWTClaimsSet;
-import com.nimbusds.jwt.SignedJWT;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import com.nimbusds.jose.*;
+import com.nimbusds.jose.crypto.MACSigner;
+import com.nimbusds.jose.crypto.MACVerifier;
+import lombok.SneakyThrows;
 
-import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 
 public class JwtTests {
+    //使用uuid生成密钥
+    private  final String secret= UUID.randomUUID().toString();
+    //用户数据的key
+    private  final String usernameKey="usernameKey";
 
-    private RSAKey rsaKey;
-    private String token;
-    private RSAKey publicJWK;
+    public  String createJwtToken(String username) throws Exception {
+        //创建头部对象
+        JWSHeader jwsHeader =
+                new JWSHeader.Builder(JWSAlgorithm.HS256)       // 加密算法
+                        .type(JOSEObjectType.JWT) // 静态常量
+                        .build();
 
-    @BeforeEach
-    public void createKey() {
-        try {
-            rsaKey = new RSAKeyGenerator(2048).generate();
-            publicJWK = rsaKey.toPublicJWK();
-            System.err.println("rsaKey=" + rsaKey);
-            System.err.println("publicJWK=" + publicJWK);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        //创建载荷
+        Map<String,Object> map=new HashMap<String,Object>();
+        map.put(usernameKey, username);
+        Payload payload= new Payload(map);
+
+        //创建签名器
+        JWSSigner jwsSigner = new MACSigner(secret);//密钥
+
+        //创建签名
+        JWSObject jwsObject = new JWSObject(jwsHeader, payload);// 头部+载荷
+        jwsObject.sign(jwsSigner);//再+签名部分
+
+        //生成token字符串
+        return jwsObject.serialize();
     }
 
-    @Test
-    public void createToken() {
-        try {
-            RSASSASigner signer = new RSASSASigner(rsaKey);
-            JWTClaimsSet claimsSet = new JWTClaimsSet.Builder()//
-                    .subject("userlogin")//
-                    .issuer("张维维")//
-                    .issueTime(new Date())//
-                    .expirationTime(new Date(System.currentTimeMillis() + 1 * 60 * 1000))//
-                    .jwtID(UUID.randomUUID().toString())//
-                    .claim("username", "zhangweiwei")//
-                    .build();
-            SignedJWT signedJWT = new SignedJWT(new JWSHeader(JWSAlgorithm.RS256), claimsSet);
-            signedJWT.sign(signer);
-            token = signedJWT.serialize();
-            System.err.println("token=" + token);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    public  boolean verify(String jwtStr) throws Exception {
+        JWSObject jwsObject=JWSObject.parse(jwtStr);
+        JWSVerifier jwsVerifier=new MACVerifier(secret);
+        return jwsObject.verify(jwsVerifier);
     }
 
-    @AfterEach
-    public void checkToken() {
-        try {
-            SignedJWT signedJWT = SignedJWT.parse(token);
-            RSASSAVerifier verifier = new RSASSAVerifier(publicJWK);
-            signedJWT.verify(verifier);
-            JWTClaimsSet claimsSet = signedJWT.getJWTClaimsSet();
-            System.err.println(claimsSet.getStringClaim("username"));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    public  String getUserNameFormJwt(String jwtStr) throws Exception{
+        JWSObject jwsObject=JWSObject.parse(jwtStr);
+        Map<String,Object> map=jwsObject.getPayload().toJSONObject();
+        return (String) map.get(usernameKey);
+    }
+
+    public static void main(String[] args) throws Exception {
+        System.err.println(new JwtTests().createJwtToken("hello world"));
     }
 }
