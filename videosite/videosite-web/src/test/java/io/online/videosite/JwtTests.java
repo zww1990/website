8417f9552c187eaa95cd00919add9cb0f6d9f358
zww1990 <zww1990@foxmail.com>
2024-07-25 09:15:32 +0800

    JwtTests.java

--- videosite/videosite-web/src/test/java/io/online/videosite/JwtTests.java
+++ videosite/videosite-web/src/test/java/io/online/videosite/JwtTests.java
@@ -3,7 +3,6 @@ package io.online.videosite;
 import com.nimbusds.jose.*;
 import com.nimbusds.jose.crypto.MACSigner;
 import com.nimbusds.jose.crypto.MACVerifier;
-import lombok.SneakyThrows;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -11,21 +10,19 @@ import java.util.UUID;
 
 public class JwtTests {
     //使用uuid生成密钥
-    private  final String secret= UUID.randomUUID().toString();
-    //用户数据的key
-    private  final String usernameKey="usernameKey";
+    private final String secret = UUID.randomUUID().toString();
 
-    public  String createJwtToken(String username) throws Exception {
+    /**
+     * 创建jwt令牌
+     */
+    public String createJwtToken(Map<String, Object> jsonObject) throws Exception {
         //创建头部对象
-        JWSHeader jwsHeader =
-                new JWSHeader.Builder(JWSAlgorithm.HS256)       // 加密算法
+        JWSHeader jwsHeader = new JWSHeader.Builder(JWSAlgorithm.HS256) // 加密算法
                         .type(JOSEObjectType.JWT) // 静态常量
                         .build();
 
         //创建载荷
-        Map<String,Object> map=new HashMap<String,Object>();
-        map.put(usernameKey, username);
-        Payload payload= new Payload(map);
+        Payload payload = new Payload(jsonObject);
 
         //创建签名器
         JWSSigner jwsSigner = new MACSigner(secret);//密钥
@@ -38,19 +35,36 @@ public class JwtTests {
         return jwsObject.serialize();
     }
 
-    public  boolean verify(String jwtStr) throws Exception {
-        JWSObject jwsObject=JWSObject.parse(jwtStr);
-        JWSVerifier jwsVerifier=new MACVerifier(secret);
+    /**
+     * 验证jwt令牌
+     */
+    public boolean verify(String jwtStr) throws Exception {
+        JWSObject jwsObject = JWSObject.parse(jwtStr);
+        JWSVerifier jwsVerifier = new MACVerifier(secret);
         return jwsObject.verify(jwsVerifier);
     }
 
-    public  String getUserNameFormJwt(String jwtStr) throws Exception{
-        JWSObject jwsObject=JWSObject.parse(jwtStr);
-        Map<String,Object> map=jwsObject.getPayload().toJSONObject();
-        return (String) map.get(usernameKey);
+    /**
+     * 获取jwt载体
+     */
+    public Map<String, Object> getPayload(String jwtStr) throws Exception {
+        JWSObject jwsObject = JWSObject.parse(jwtStr);
+        return jwsObject.getPayload().toJSONObject();
     }
 
     public static void main(String[] args) throws Exception {
-        System.err.println(new JwtTests().createJwtToken("hello world"));
+        JwtTests jwt = new JwtTests();
+
+        Map<String, Object> map = new HashMap<>();
+        map.put("text", "hello world");
+
+        String token = jwt.createJwtToken(map);
+        System.out.println("token = " + token);
+
+        boolean verify = jwt.verify(token);
+        System.out.println("verify = " + verify);
+
+        Map<String, Object> payload = jwt.getPayload(token);
+        System.out.println("payload = " + payload);
     }
 }
